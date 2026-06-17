a36b1c7328580693e3b0daabcd2d8275a781c264
qexo4 <qexo4444@gmail.com>
2026-06-17 18:27:40 +0200

    Update config.php

--- config.php
+++ config.php
@@ -29,23 +29,14 @@ try {
 // Funkcja wysyłająca powiadomienia na Discord (Wersja Poprawiona)
 function sendDiscordMessage($message) {
     $webhookUrl = "https://discord.com/api/webhooks/1512507843801124876/JB3O32EtcgKbUmBDjnTY9kJAmszKY7oIVS9FLin6bKsEsPZOmN1fxGZeJ_XT2xIUucNb";
-
-    $json_data = json_encode([
-        "content" => $message,
-        "username" => "CS-Manager Bot",
-        "tts" => false
-    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
-    $ch = curl_init($webhookUrl);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data); // <--- Tutaj była poprawiona linijka
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    $response = curl_exec($ch);
+$response = curl_exec($ch);
+    $error = curl_error($ch); // Sprawdzamy błąd
     curl_close($ch);
+    
+    // Jeśli jest błąd, wyświetl go na ekranie
+    if ($error) {
+        die("BŁĄD CURL: " . $error);
+    }
     return $response;
 }
 ?>
